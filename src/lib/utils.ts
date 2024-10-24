4a16a046ad98dd67baded1074338647880191805
samalsubrat <samalsubrat919@gmail.com>
2024-10-24 13:37:59 +0530

    metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,38 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from 'next'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = 'Linky - URL Context Chatbot',
+  description = 'Chat with your links in seconds.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@samalsubrat',
+    },
+    icons,
+    metadataBase: new URL("http://localhost:3000/")
+  }
 }
